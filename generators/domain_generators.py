620fdf2d593501b119202a54a2b3913bb0ea3641
Jerry Zhang <u7305891@anu.edu.au>
2023-09-14 05:25:23 +1000

    finish domain_generators.py for pddl instance grid

--- generators/domain_generators.py
+++ generators/domain_generators.py
@@ -63,13 +63,83 @@ class BlocksWorldGenerator(PromptGenerator):
         return ""
 
 class Navigation2DGenerator(PromptGenerator):
+    grid_domain_pddl_text = """
+(define (domain grid-navigation)
+(:requirements :strips :typing)
+
+(:types
+    cell
+)
+
+(:predicates
+    (agent-at ?pos - cell)
+    (obstacle-at ?pos - cell)  ; Obstacle-at predicate
+    (neighbors ?pos1 - cell ?pos2 - cell)
+)
+
+(:action move
+    :parameters (?from - cell ?to - cell)
+    :precondition (and (agent-at ?from) (neighbors ?from ?to) (not (obstacle-at ?to))) 
+    :effect (and (not (agent-at ?from)) (agent-at ?to))
+)
+)
+"""
+    def generate_problem_pddl(self, size, initial, goal, obstacles):
+        width, height = map(int, size.split(","))
+        init_x, init_y = map(int, initial.split(","))
+        goal_x, goal_y = map(int, goal.split(","))
+        
+        # Generate objects
+        objects = []
+        for x in range(width):
+            for y in range(height):
+                objects.append(f"cell-{x}-{y}")
+        objects_str = " ".join(objects)
+        
+        # Generate init states
+        init_states = []
+        init_states.append(f"(agent-at cell-{init_x}-{init_y})")
+        
+        # Generate neighbors
+        for x in range(width):
+            for y in range(height):
+                if x + 1 < width:
+                    init_states.append(f"(neighbors cell-{x}-{y} cell-{x+1}-{y})")
+                    init_states.append(f"(neighbors cell-{x+1}-{y} cell-{x}-{y})") # ensure the reverse also holds
+                if y + 1 < height:
+                    init_states.append(f"(neighbors cell-{x}-{y} cell-{x}-{y+1})")
+                    init_states.append(f"(neighbors cell-{x}-{y+1} cell-{x}-{y})")
+        
+        # Generate obstacles
+        for obstacle in obstacles:
+            obs_x, obs_y = map(int, obstacle.split(","))
+            init_states.append(f"(obstacle-at cell-{obs_x}-{obs_y})")  
+        
+        init_states_str = "\n    ".join(init_states)
+        
+        # Generate the full PDDL string
+        problem_pddl = f"""(define (problem grid-problem)
+(:domain grid-navigation)
+(:objects
+    {objects_str}
+)
+(:init
+    {init_states_str}
+)
+(:goal (agent-at cell-{goal_x}-{goal_y}))
+)"""
+    
+        return problem_pddl
+    
+
     def get_domain_text(self):
         match self.representation:
             case "nl-casual":
                 return "You are a robot in a discrete finite grid. You start at a certain position and aim to reach the goal position. However, you can only move one square at a time and you cannot move diagonally. There may be obstacles in your path, which you cannot move onto or over. Coordinates are represented as (x,y)."
             case "nl-math":
                 return "Let r denote a discrete, non-negative 2-dimensional point. The value of r is initially set to value s. A transition t is a list of two points where the distance between them is exactly one. Transitions cannot include a point that is in list o."
-
+            case "pddl":
+                return "You are currently solving the below planning problem represented by two files: a domain.pddl and a problem.pddl. \ Below is the domain.pddl file: \n" + self.grid_domain_pddl_text + "\\"
 
         return ""
     
@@ -84,7 +154,9 @@ class Navigation2DGenerator(PromptGenerator):
                 return f'The grid is {grid_size[0]} wide and {grid_size[1]} tall. You are initially located at ({initial_pos}) and you need to get to ({goal_pos}). {self.get_obstacles(obstacles)}\n Please reply only with the sequence of coordinates that you visit.'
             case "nl-math":
                 return f'r may move no further than {int(grid_size[0]) - 1} on the x-axis and {int(grid_size[1]) - 1} on the y-axis. Let o={self.get_obstacles(obstacles)}. Given s=({initial_pos}) and g=({goal_pos}), what is the sequence of valid transitions needed to move r to g? Please reply only with this sequence.'
-            
+            case "pddl":
+                return self.generate_problem_pddl(grid_size, initial_pos, goal_pos, obstacles)
+
         return ""
 
     def get_obstacles(self, obstacles):
