a6309db22ec628fe2a5442dc813ac7463755a915
Yuechen Liu <u7227895@anu.edu.au>
2023-09-14 01:37:03 +1000

    changed visualisation output & add in validity check

--- visualization.py
+++ visualization.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw
 import os
+import re
 
 # Configure the outputted image
 img_size = (3072, 1024)
@@ -41,11 +42,11 @@ def color_cell(draw, padding, cells, x, y, color):
     cell = get_grid_pos(x, y, padding, cells)
     draw.rectangle(cell, fill=color)
 
-def draw_circle(draw, padding, cells, x, y, color):
+def draw_circle(draw, padding, cells, x, y, margin, color):
     cell = get_grid_pos(x, y, padding, cells)
 
     # 5% of cell size
-    circle_padding = 0.05 * (cell[2] - cell[0])
+    circle_padding = margin * (cell[2] - cell[0])
 
     left = cell[0] + circle_padding
     top = cell[1] + circle_padding
@@ -55,18 +56,35 @@ def draw_circle(draw, padding, cells, x, y, color):
     draw.ellipse([left, top, right, bottom], fill=color)
 
 
+def draw_path(draw, padding, cells, path_coords, color, thickness=10):
+    for i in range(1, len(path_coords)):
+        # Get the center of the start cell
+        start_cell = get_grid_pos(path_coords[i-1][0], path_coords[i-1][1], padding, cells)
+        start_center = ((start_cell[0] + start_cell[2]) / 2, (start_cell[1] + start_cell[3]) / 2)
 
-def generate_image(cells, initial_pos, goal_pos, obstacles, output_filename="img-out/output_image.png"):
+        # Get the center of the end cell
+        end_cell = get_grid_pos(path_coords[i][0], path_coords[i][1], padding, cells)
+        end_center = ((end_cell[0] + end_cell[2]) / 2, (end_cell[1] + end_cell[3]) / 2)
+
+        # Draw the line
+        draw.line([start_center, end_center], fill=color, width=thickness)
+
+def generate_image(cells, initial_pos, goal_pos, obstacles, response = "", output_filename="img-out/output_image.png"):
     img = Image.new("RGB", img_size, "white")
     draw = ImageDraw.Draw(img)
 
-    draw_circle(draw, padding, cells, initial_pos[0], initial_pos[1], red_color)
+    draw_circle(draw, padding, cells, initial_pos[0], initial_pos[1], 0.05, red_color)
     color_cell(draw, padding, cells, goal_pos[0], goal_pos[1], aqua_color)
 
     for obstacle in obstacles:
         color_cell(draw, padding, cells, obstacle[0], obstacle[1], black_color)
 
+        draw_path(draw, padding, cells, response, aqua_color)
+
     draw_grid(draw, padding, cells, 5)
+
+    for coord in response:
+        draw_circle(draw, padding, cells, coord[0], coord[1], 0.40, aqua_color)
 
     os.makedirs(os.path.dirname(output_filename), exist_ok=True)
     img.save(output_filename)
