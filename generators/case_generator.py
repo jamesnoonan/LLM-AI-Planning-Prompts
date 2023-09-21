33277fd2210edf371258aaa6265bdb427afeae11
James Noonan <43080771+jamesnoonan@users.noreply.github.com>
2023-09-21 16:00:50 +1000

    feat: only generate solvable problems

--- generators/case_generator.py
+++ generators/case_generator.py
@@ -8,6 +8,34 @@ def pos_tuple_to_string(tuple):
 def generate_random_pos(size):
     return (random.randrange(0, size), random.randrange(0, size))
 
+def is_valid_problem(size, initial, goal, obstacles):
+    # Breadth-first search algorithm to find path
+    visited = []
+    queue = [initial]
+
+    actions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    while len(queue) > 0:
+        point = queue.pop()
+
+        # Path has been found so return
+        if point == goal:
+            return True
+
+        for dx, dy in actions:
+            x, y = point[0] + dx, point[1] + dy
+            next_point = (x, y)
+
+            is_in_grid = 0 < x and x < size and 0 < y and y < size
+            is_free_point = next_point not in obstacles and next_point not in visited
+
+            if is_in_grid and is_free_point:
+                visited.append(next_point)
+                queue.insert(0, next_point)
+
+    # Was unable to find path
+    return False
+
 def generate_navigation2d(size, obstacle_count):
     if (obstacle_count < 0):
         max_obstacles = size * size - 2 * size
@@ -25,7 +53,8 @@ def generate_navigation2d(size, obstacle_count):
         # If valid config, we can break
         has_dup_obstacle = len(obstacles) != len(set(obstacles))
         if not (initial == goal or initial in obstacles or goal in obstacles or has_dup_obstacle):
-            break
+            if (is_valid_problem(size, initial, goal, obstacles)): # Only use if there is a valid path
+                break
 
 
     return {
